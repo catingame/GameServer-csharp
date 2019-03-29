35f25e5071fad088747dfbe0cc424da07f22629d
keicoon <keicoon15@gmail.com>
2019-03-29 10:41:00 +0900

    Fix errors in my logger class

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -1,57 +1,60 @@
 ﻿using System;
-using Logger;
+using System.Collections.Generic;
+using System.Text;
 using CommandLine;
+using Core;
 
 namespace Server
 {
-    class Options
+    class ConsoleServerOptions
     {
-        [Option("p", "port", Required = true, HelpText = "Input server port.", Default = 9339)]
-        public Int32 port { get; set; }
+        [Option('p', "port", Required = true, HelpText = "Input server port.", Default = 9339)]
+        public Int32 Port { get; set; }
 
-        [Option("v", null, HelpText = "Print details during execution.")]
+        [Option('v', null, HelpText = "Print details during execution.")]
         public Boolean Verbose { get; set; }
-
-        [HelpOption(HelpText = "Display this help screen.")]
-        public String GetUsage()
-        {
-            var usage = new StringBuilder();
-            usage.AppendLine("ConsoleServer Application 1.0");
-            usage.AppendLine("Read user manual for usage instructions...");
-            return usage.ToString();
-        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var options = new Options();
-            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            Parser.Default.ParseArguments<ConsoleServerOptions>(args)
+                .WithParsed(RunServer)
+                .WithNotParsed(HandleErrors);
+        }
+
+        private static void RunServer(ConsoleServerOptions options)
+        {
+            Logger.SetLogger(
+                Logger.GetLoggerFactory()
+                    .UseNLog("NLog.Config")
+                    .Create<Program>()
+            );
+
+            if (options.Verbose)
             {
-                if(options.Verbose)
-                {
-                    Logger.SetLevel(LogType.Info);
-                }
-
-                Logger.WriteLine(LogType.Info, "Try to start server.");
-
-                Server.Start(options.port).Next(result => {
-                    if(result.fail) 
-                    {
-                        // @something
-                        // @error message
-                        Logger.WriteLine(LogType.Error, "");
-                        throw new Exception("");
-                    }
-
-                    Game.Init();
-                    Logger.WriteLine(LogType.Info, $"Run Server in port {options.port}");
-                });
+                Logger.SetLevel(LogType.Info);
             }
-            else
-            {
-                Console.WriteLine(options.GetUsage());
-            }
+
+            Logger.Write(LogType.Info, "Try to start server.");
+
+            //Server.Start(options.Port).Next(result => {
+            //    if (result.fail)
+            //    {
+            //        // @something
+            //        // @error message
+            //        Logger.WriteLine(LogType.Error, "");
+            //        throw new Exception("");
+            //    }
+
+            //    Game.Init();
+            //    Logger.WriteLine(LogType.Info, $"Run Server in port {options.port}");
+            //});
+        }
+
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            Console.WriteLine(errors.ToString());
         }
     }
 }
--- Core/Log/Logger.cs
+++ Core/Log/Logger.cs
@@ -1,34 +1,73 @@
 using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace Core
 {
     public enum LogType
     {
-        Info = 0,
-        Warning = 1,
-        Error = 2
+        Trace = LogLevel.Trace,
+        Debug = LogLevel.Debug,
+        Info = LogLevel.Information,
+        Warning = LogLevel.Warning,
+        Error = LogLevel.Error,
+        Fatal = LogLevel.Critical,
+        None = LogLevel.None
+    }
+
+    public static class LoggerExtension
+    {
+        public static ILoggerFactory UseNLog(this ILoggerFactory factory, String configFile)
+        {
+            NLog.LogManager.LoadConfiguration(configFile);
+            return factory.AddNLog();
+        }
+        public static ILogger Create<T>(this ILoggerFactory factory)
+        {
+            return factory.CreateLogger<T>();
+        }
     }
 
     public class Logger
     {
-        public static Logger _handle = new Logger();
-        private Logger() {}
-        private LogType _level = LogType.Info;
-        
+        private static ILogger _logger;
+        private static readonly ILoggerFactory _factory = new LoggerFactory();
+        private static LogLevel _curLogLevel;
+
+        public static ILoggerFactory GetLoggerFactory()
+        {
+            return _factory;
+        }
+
+        public static ILogger Create<T>()
+        {
+            return _factory.CreateLogger<T>();
+        }
+
+        public static void SetLogger(ILogger logger)
+        {
+            Interlocked.Exchange(ref _logger, logger);
+        }
+
+        public static void SetLevel(LogType type)
+        {
+            _curLogLevel = (LogLevel)type;
+        }
+
         /* method */
 
-        public void SetLevel(LogType logLevel)
+        public static void Write(LogType type, String message, params Object[] args)
         {
-            _level = logLevel;
-        }
+            var originLogLevelType = (LogLevel)type;
+            var logLevel = (originLogLevelType - _curLogLevel);
 
-        public void WriteLine(LogType type, string message)
-        {
-            var level = (type - _level);
-            if(level >= 0)
+            if(_logger != null && logLevel >= 0)
             {
-                Console.WriteLine(message);
+                _logger.Log(originLogLevelType, message, args);
             }
         }
+
+        // TODO: need to implement more variety method like Exception, eventId
     }
 }
