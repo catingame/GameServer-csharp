6e5fa94d041a4eee88e948f217bd003472066d80
keicoon <keicoon15@gmail.com>
2019-03-30 20:02:22 +0900

    Add basic DB class

---
+++ Core/DB/IDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DB
+{
+    public interface IDb
+    {
+        IDb Connect(DbOption Option);
+        Boolean SetDb(String Name);
+        String Get(String Key);
+        void Set(String Key, String Value);
+    }
+
+    public class DbOption
+    {
+        public String Address;
+        public Int32 Port;
+        public Boolean bAsync;
+    }
+}
---
+++ Core/DB/Redis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Core.DB
+{
+    class Redis : IDb
+    {
+        ConnectionMultiplexer _redis = null;
+        IDatabase _db = null;
+        Object _asyncObject = null;
+
+        public Redis(String Address, Int32 Port, Boolean bAsync)
+        {
+            var dbAddress = $"{Address}:{Port}";
+            _redis = ConnectionMultiplexer.Connect(dbAddress) 
+                ?? throw new Exception($"Connection fail Db={dbAddress}");
+            _asyncObject = bAsync ? new Object() : null;
+        }
+
+        public IDb Connect(DbOption Option)
+        {
+            return new Redis(Option.Address, Option.Port, Option.bAsync);
+        }
+
+        public Boolean SetDb(String Name)
+        {
+            if(Int32.TryParse(Name, out var DbID))
+            { 
+                _db = _redis.GetDatabase(DbID, _asyncObject);
+                return true;
+            }
+            return false;
+        }
+
+        public String Get(String Key)
+        {
+            return _db?.StringGet(Key);
+        }
+
+        public void Set(String Key, String Value)
+        {
+            _db?.StringSet(Key, Value);
+        }
+    }
+}
