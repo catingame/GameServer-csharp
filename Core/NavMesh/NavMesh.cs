02101cee28d3f50a6644a7747cbdfcebf33f7a82
keicoon <keicoon15@gmail.com>
2021-01-31 06:36:50 +0900

    Update NavMesh
    - Add NavMesh class

---
+++ Core/NavMesh/NavMesh.cs
@@ -0,0 +1,159 @@
+﻿using SharpNav;
+using SharpNav.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Core.NavMesh
+{
+	enum ENavLevelDataType
+    {
+		OBJ
+    }
+    internal class NavMesh
+    {
+		private class AreaIdGenerationSettings
+		{
+			public float MaxTriSlope { get; set; }
+			public float MinLevelHeight { get; set; }
+			public float MaxLevelHeight { get; set; }
+		}
+
+		private NavMeshGenerationSettings settings;
+		private AreaIdGenerationSettings areaSettings;
+
+		private Heightfield heightfield;
+		private CompactHeightfield compactHeightfield;
+		private ContourSet contourSet;
+		private PolyMesh polyMesh;
+		private PolyMeshDetail polyMeshDetail;
+
+		private Boolean hasGenerated = false;
+
+		private NavMesh(in Triangle3[] levelTris)
+        {
+			GenerateNavMesh(levelTris);
+		}
+
+        public static NavMesh GetNavMeshFromLevelData(String levelDataPath, ENavLevelDataType dataType = ENavLevelDataType.OBJ)
+        {
+			switch(dataType)
+            {
+				case ENavLevelDataType.OBJ:
+					var level = new ObjModel("nav_test.obj");
+					var triangles = level.GetTriangles();
+					var navMesh = new NavMesh(triangles);
+					return navMesh;
+			}
+
+			throw new Exception(); // TODO:
+        }
+
+        private void GenerateNavMesh(in Triangle3[] levelTris)
+        {
+			Console.WriteLine("Generating NavMesh");
+
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			long prevMs = 0;
+			try
+			{
+				//level.SetBoundingBoxOffset(new SVector3(settings.CellSize * 0.5f, settings.CellHeight * 0.5f, settings.CellSize * 0.5f));
+				var triEnumerable = TriangleEnumerable.FromTriangle(levelTris, 0, levelTris.Length);
+				BBox3 bounds = triEnumerable.GetBoundingBox();
+
+				heightfield = new Heightfield(bounds, settings);
+
+				Console.WriteLine("Heightfield");
+				Console.WriteLine(" + Ctor\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				/*Area[] areas = AreaGenerator.From(triEnumerable, Area.Default)
+					.MarkAboveHeight(areaSettings.MaxLevelHeight, Area.Null)
+					.MarkBelowHeight(areaSettings.MinLevelHeight, Area.Null)
+					.MarkBelowSlope(areaSettings.MaxTriSlope, Area.Null)
+					.ToArray();
+				heightfield.RasterizeTrianglesWithAreas(levelTris, areas);*/
+				heightfield.RasterizeTriangles(levelTris, Area.Default);
+
+				Console.WriteLine(" + Rasterization\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				Console.WriteLine(" + Filtering");
+				prevMs = sw.ElapsedMilliseconds;
+
+				heightfield.FilterLedgeSpans(settings.VoxelAgentHeight, settings.VoxelMaxClimb);
+
+				Console.WriteLine("   + Ledge Spans\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				heightfield.FilterLowHangingWalkableObstacles(settings.VoxelMaxClimb);
+
+				Console.WriteLine("   + Low Hanging Obstacles\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				heightfield.FilterWalkableLowHeightSpans(settings.VoxelAgentHeight);
+
+				Console.WriteLine("   + Low Height Spans\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				compactHeightfield = new CompactHeightfield(heightfield, settings);
+
+				Console.WriteLine("CompactHeightfield");
+				Console.WriteLine(" + Ctor\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				compactHeightfield.Erode(settings.VoxelAgentRadius);
+
+				Console.WriteLine(" + Erosion\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				compactHeightfield.BuildDistanceField();
+
+				Console.WriteLine(" + Distance Field\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				compactHeightfield.BuildRegions(0, settings.MinRegionSize, settings.MergedRegionSize);
+
+				Console.WriteLine(" + Regions\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				//Random r = new Random();
+				//regionColors = new Color4[compactHeightfield.MaxRegions];
+				//regionColors[0] = Color4.Black;
+				//for (int i = 1; i < regionColors.Length; i++)
+				//	regionColors[i] = new Color4((byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255), 255);
+
+				Console.WriteLine(" + Colors\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				contourSet = compactHeightfield.BuildContourSet(settings);
+
+				Console.WriteLine("ContourSet");
+				Console.WriteLine(" + Ctor\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				polyMesh = new PolyMesh(contourSet, settings);
+
+				Console.WriteLine("PolyMesh");
+				Console.WriteLine(" + Ctor\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				polyMeshDetail = new PolyMeshDetail(polyMesh, compactHeightfield, settings);
+
+				Console.WriteLine("PolyMeshDetail");
+				Console.WriteLine(" + Ctor\t\t\t\t" + (sw.ElapsedMilliseconds - prevMs).ToString("D3") + " ms");
+				prevMs = sw.ElapsedMilliseconds;
+
+				hasGenerated = true;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Navmesh generation failed with exception:" + Environment.NewLine + e.ToString());
+			}
+			finally
+			{
+				sw.Stop();
+			}
+		}
+    }
+}
